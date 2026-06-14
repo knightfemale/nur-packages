83733f669183f631c3005f3c0e4897e1df4f3fc6
骑士姬 <2067834160@qq.com>
2026-06-14 18:11:31 +0800

    feat: 导入 nix-cachyos-kernel

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
       url = "github:hercules-ci/flake-parts/main";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/master";
   };
   outputs = inputs: import ./outputs inputs;
 }
