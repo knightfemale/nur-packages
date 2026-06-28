1540aa3e5a3540c0c1a81d3313ae6f04d6e5f4ba
骑士姬 <2067834160@qq.com>
2026-06-28 15:29:46 +0800

    feat: 添加并更新 kernels

--- outputs/packages/kernel/cachyos.nix
+++ outputs/packages/kernel/cachyos.nix
@@ -12,12 +12,24 @@ lib.optionalAttrs (cachyos-kernel != null) {
     name = "cachyos-bmq-lto";
     packages = cachyos-kernel.linuxPackages-cachyos-bmq-lto;
   };
-  server-lto = {
-    name = "cachyos-server-lto";
-    packages = cachyos-kernel.linuxPackages-cachyos-server-lto;
+  bore-lto-v2 = {
+    name = "cachyos-bore-lto-v2";
+    packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-x86_64-v2;
   };
   bore-lto-v3 = {
     name = "cachyos-bore-lto-v3";
     packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-x86_64-v3;
+  };
+  bore-lto-v4 = {
+    name = "cachyos-bore-lto-v4";
+    packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-x86_64-v4;
+  };
+  bore-lto-zen4 = {
+    name = "cachyos-bore-lto-zen4";
+    packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-zen4;
+  };
+  server-lto = {
+    name = "cachyos-server-lto";
+    packages = cachyos-kernel.linuxPackages-cachyos-server-lto;
   };
 }
