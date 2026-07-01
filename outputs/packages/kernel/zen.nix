5631e330bd85870bd33ad8301605ee4c01398efa
骑士姬 <2067834160@qq.com>
2026-07-01 10:00:31 +0800

    chore: 更新 linux 内核

--- outputs/packages/kernel/zen.nix
+++ outputs/packages/kernel/zen.nix
@@ -6,16 +6,16 @@ let
     main = rec {
       name = "zen";
       version = "7.1.2";
-      modDirVersion = "${version}-${name}1";
+      modDirVersion = "${version}-${name}2";
       url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-      sha256 = "sha256-vyN0VQhyZVEQSTC5hmv++kbc63KqhlHesec36397hPU=";
+      sha256 = "sha256-Vk62dxJ4gEod6XwJit8GaNX6EVgEx6fiZkroPhPuL2s=";
     };
     lqx = rec {
       name = "lqx";
       version = "7.0.13";
-      modDirVersion = "${version}-${name}1";
+      modDirVersion = "${version}-${name}3";
       url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-      sha256 = "sha256-ZL1UM6t+WJpFUg7+Lzwb3GEHVaGUZylkBFD7LYUmCfk=";
+      sha256 = "sha256-ahxKr/HMwzB0tDwjHHETjEsVQnO9qjlCSmMGIVnHcR4=";
     };
   };
 in
