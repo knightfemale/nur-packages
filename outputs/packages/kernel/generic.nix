19c9789c6141b6be680a9aeb80607b4adfa35ca6
骑士姬 <2067834160@qq.com>
2026-07-19 17:11:17 +0800

    chore: 更新 linux 内核

--- outputs/packages/kernel/generic.nix
+++ outputs/packages/kernel/generic.nix
@@ -5,10 +5,10 @@ let
   kernel = {
     main = rec {
       name = "generic";
-      version = "7.1.3";
+      version = "7.1.4";
       modDirVersion = "${version}";
       url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${modDirVersion}.tar.xz";
-      sha256 = "sha256-vkHAaOiPUkKhm8zb/74HexjEe0X2J+IyVQS0+red0dw=";
+      sha256 = "sha256-HGOSKhGWddOOOuD49u4H8VxBp4arntZlY3SbuMmgji4=";
     };
   };
 in
