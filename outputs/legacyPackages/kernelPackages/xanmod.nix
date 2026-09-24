ec0c72536f0ce8f945f4412c5a3b923b25571870
骑士姬 <2067834160@qq.com>
2026-09-24 08:35:14 +0800

    chore: 更新 linux 内核

--- outputs/legacyPackages/kernelPackages/xanmod.nix
+++ outputs/legacyPackages/kernelPackages/xanmod.nix
@@ -5,24 +5,24 @@ let
   kernel = {
     edge = rec {
       name = "xanmod";
-      version = "7.2.6";
+      version = "7.2.7";
       modDirVersion = "${version}-${name}1";
       url = "https://gitlab.com/xanmod/linux/-/archive/${modDirVersion}.tar.bz2";
-      sha256 = "sha256-ZJE+vsWyAsZ0MzZ9ahimSPFGpL+goCiV+MsVHgpeqoA=";
+      sha256 = "sha256-KRxHXKpQx1bMXIO2R6p3D7NYbetIKpMEl+td4zImBq0=";
     };
     lts = rec {
       name = "xanmod";
-      version = "6.18.52";
+      version = "6.18.53";
       modDirVersion = "${version}-${name}1";
       url = "https://gitlab.com/xanmod/linux/-/archive/${modDirVersion}.tar.bz2";
-      sha256 = "sha256-/CYiJTg9tBopkWNbNHgPbWoqMfbb+eZZNYN8uJYJQyY=";
+      sha256 = "sha256-jhAL987XEqaipsgeP6Hiev+jLew6VCi3rtQjvGjEBmo=";
     };
     main = rec {
       name = "xanmod";
-      version = "7.2.6";
+      version = "7.2.7";
       modDirVersion = "${version}-${name}1";
       url = "https://gitlab.com/xanmod/linux/-/archive/${modDirVersion}.tar.bz2";
-      sha256 = "sha256-ZJE+vsWyAsZ0MzZ9ahimSPFGpL+goCiV+MsVHgpeqoA=";
+      sha256 = "sha256-KRxHXKpQx1bMXIO2R6p3D7NYbetIKpMEl+td4zImBq0=";
     };
     rt = rec {
       name = "rt-xanmod";
--- outputs/legacyPackages/kernelPackages/zen.nix
+++ outputs/legacyPackages/kernelPackages/zen.nix
@@ -5,17 +5,17 @@ let
   kernel = {
     lqx = rec {
       name = "lqx";
-      version = "7.2.6";
+      version = "7.2.7";
       modDirVersion = "${version}-${name}1";
       url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-      sha256 = "sha256-x6y5vwiHKNxCPLJmwuouYnim7Slq88+YgQ3Deem3xLE=";
+      sha256 = "sha256-sFeqZeqzA9LBAgpXZU2DZcsb3Yi3DeUiQdUKQvD5Pvw=";
     };
     main = rec {
       name = "zen";
-      version = "7.2.6";
-      modDirVersion = "${version}-${name}2";
+      version = "7.2.7";
+      modDirVersion = "${version}-${name}1";
       url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-      sha256 = "sha256-N+V5UR8ViVg2aI2vl96Jc4O9DwYViClAMyYe3JENcjc=";
+      sha256 = "sha256-JzhQuxhFW+8RqlXQ9xKRDT689WQIkZLUeVLvB0xPPHY=";
     };
   };
 in
